e94e0e0c72e7a7406b569e24c8892bc3a2856123
Nurunnahar <nurunnahar9081@gmail.com>
2023-10-21 00:08:27 +0600

    project create done

--- mrx/app/Http/Controllers/Backend/ProjectController.php
+++ mrx/app/Http/Controllers/Backend/ProjectController.php
@@ -17,4 +17,36 @@ class ProjectController extends Controller
         return Project::latest()->get();
     }
 
+ 
+    public function createproject(Request $request)
+    {
+        // Get files from the request using the field names from   form
+        $preImg_file = $request->file('previewLink');
+        $thmImg_file = $request->file('thumbnailLink');
+
+        // Define upload destinations
+        $uploadPath = 'uploads/project/';
+
+        // Move and store the uploaded files
+        $preImg_fileName = hexdec(uniqid()) . '.' . $preImg_file->getClientOriginalExtension();
+        $preImg_file->move(public_path($uploadPath), $preImg_fileName);
+        $preImg_filePath = $uploadPath . $preImg_fileName;
+
+        $thmImg_fileName = hexdec(uniqid()) . '.' . $thmImg_file->getClientOriginalExtension();
+        $thmImg_file->move(public_path($uploadPath), $thmImg_fileName);
+        $thmImg_filePath = $uploadPath . $thmImg_fileName;
+
+        // Save To Database
+        Project::create([
+            'title' => $request->input('title'),
+            'thumbnailLink' => $thmImg_filePath,
+            'previewLink' => $preImg_filePath,
+            'details' => $request->input('details'),
+        ]);
+
+        return response()->json([
+            'status' => 'ok',
+            'message' => 'Created successfully'
+        ], 201);
+    }
 }
